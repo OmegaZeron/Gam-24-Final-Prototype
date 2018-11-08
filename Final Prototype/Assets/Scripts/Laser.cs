875a63811712779aa45ebc7e05c6357ae01ae851
Greg <omegazeron@yahoo.com>
2018-11-08 02:32:52 -0800

    Added basic laser stuff

---
+++ Final Prototype/Assets/Scripts/Laser.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+	public enum LaserColor
+	{
+		White,
+		Allan,
+		Please,
+		Add,
+		Colors
+	}
+	private LaserColor laserColor = LaserColor.White;
+
+	private LineRenderer line;
+	
+	[SerializeField] private float laserDistance;
+	[SerializeField] private float damage;
+
+	private void Start()
+	{
+		line = GetComponent<LineRenderer>();
+		line.SetPosition(0, transform.parent.position);
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, transform.forward, out hit, laserDistance))
+		{
+			if (hit.collider)
+			{
+				line.SetPosition(1, hit.point);
+				//check for bounce/color (create new prefab, go in new direction)
+				//check for switch/enemy
+			}
+		}
+		else
+		{
+			line.SetPosition(1, transform.parent.forward * laserDistance);
+		}
+
+		StartCoroutine(FadeLaser());
+	}
+
+	private IEnumerator FadeLaser()
+	{
+		while (line.startWidth > 0)
+		{
+			line.startWidth -= .2f * Time.deltaTime;
+			line.endWidth -= .2f * Time.deltaTime;
+			yield return null;
+		}
+		Destroy(transform.parent.gameObject);
+	}
+}
---
+++ Final Prototype/Assets/Scripts/LaserGun.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserGun : MonoBehaviour
+{
+	// [SerializeField] private GameObject laserPrefab;
+	[SerializeField] private List<AudioClip> clips;
+
+	private void Start()
+	{
+		
+	}
+	
+	private void Update()
+	{
+		
+	}
+
+	private void Fire()
+	{
+		// Laser laser = Instantiate(laserPrefab, transform.position, transform.rotation).GetComponent<Laser>();
+		int rand = Random.Range(0, clips.Count);
+		AudioSource.PlayClipAtPoint(clips[rand], transform.position);
+	}
+}
