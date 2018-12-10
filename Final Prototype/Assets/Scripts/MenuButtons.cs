c0ea2bbd39b90c20030d91410e77be9247f03450
Austin <austinw2048@gmail.com>
2018-12-10 13:12:31 -0800

    Main menu and level 4 door

---
+++ Final Prototype/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour {
+
+	public void ExitClick () {
+		Application.Quit ();
+	}
+
+	public void PlayClick () {
+		SceneManager.LoadScene (1);
+	}
+
+}
