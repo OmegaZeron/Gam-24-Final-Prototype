4f38daed0a531726c2dc4aa44963b16a47bbd26e
Austin <austinw2048@gmail.com>
2018-12-10 13:26:17 -0800

    Laser sound change, main menu music

--- Final Prototype/Assets/Scripts/MenuButtons.cs
+++ Final Prototype/Assets/Scripts/MenuButtons.cs
@@ -5,12 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour {
 
+	public AudioClip music;
+	public float musicLength;
+	private AudioSource source;
+
 	public void ExitClick () {
 		Application.Quit ();
 	}
 
 	public void PlayClick () {
 		SceneManager.LoadScene (1);
+	}
+
+	void Start () {
+		source = GetComponent<AudioSource>().GetComponent<AudioSource> ();
+		source.PlayOneShot (music, 0.7f);
+		StartCoroutine (waitOnMusic());
+	}
+
+	IEnumerator waitOnMusic () {
+		yield return new WaitForSeconds (musicLength);
+		source.PlayOneShot (music, 0.7f);
+		StartCoroutine (waitOnMusic());
 	}
 
 }
